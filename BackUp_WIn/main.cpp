148f162aed798c0e4218f293b6910cbc60ad1aa9
Jolly Maxime <maxime.p.jolly@gmail.com>
2018-03-22 20:02:30 +0100

    .2
    trying to set ico on button

--- BackUp_WIn/main.cpp
+++ BackUp_WIn/main.cpp
@@ -1,14 +1,15 @@
 #include <windows.h>
 #include <ctime>
 #include <stdio.h>
-//#include "resource.h"
-
-#define ID_BUTTON 1
-#define ID_EDIT 2
+#include "resource.h"
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-static HWND hwndEdit;
+static HWND hwndTeam;
+static HWND hwndSrcdir;
+static HWND hwndDestdir;
+static HWND hwndBufreq;
 static HWND hwndLabel;
+HICON hMyIcon;
 void CenterWindow(HWND);
 void BackUp();
 
@@ -16,14 +17,14 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 	    srand(time(NULL));
 		MSG  msg;    
 		WNDCLASSW wc = {0};
-		wc.lpszClassName = L"RNP";
+		wc.lpszClassName = L"BU";
 		wc.hInstance     = hInstance;
 		wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
 		wc.lpfnWndProc   = WndProc;
-		//wc.hIcon         = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON));
+		wc.hIcon         = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON));
 		wc.hCursor       = LoadCursor(0, IDC_ARROW);
 		RegisterClassW(&wc);
-		CreateWindowW(wc.lpszClassName, L"BackUp",	WS_OVERLAPPEDWINDOW|WS_VISIBLE, 60, 134, 600, 200, 0, 0, hInstance, 0);  
+		CreateWindowW(wc.lpszClassName, L"BackUp",	WS_OVERLAPPEDWINDOW|WS_VISIBLE, 60, 134, 522, 200, 0, 0, hInstance, 0);  
 		while (GetMessage(&msg, NULL, 0, 0)) {
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
@@ -32,11 +33,18 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+
 	static wchar_t *team =  L"Nom de l'equipe :";
 	static wchar_t *srcdir =  L"Dossier source : ";
 	static wchar_t *destdir =  L"Dossier de destination : ";
 	static wchar_t *bufreq =  L"Frequence de sauvegarde : ";
 	HWND hwndButton;
+	HWND hwndButtonB;
+	HWND hwndButtonC;
+	HWND hwndButtonD;
+	HWND hwndButtonE;
+	HINSTANCE hInstance = GetModuleHandle(NULL);
+
 	switch(msg) {
 	case WM_KEYDOWN:
 		if (wParam == VK_ESCAPE) {
@@ -48,16 +56,30 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 		}     
 		break; 
 	case WM_CREATE: 
-		hwndButton=CreateWindowW(L"Button", L"Lancer BackUp", WS_VISIBLE|WS_CHILD, 190, 130, 120, 21, hwnd, (HMENU)ID_BUTTON, NULL, NULL);
 		CenterWindow(hwnd);
+
+		hMyIcon=LoadIcon(hInstance,MAKEINTRESOURCE(IDI_MYICONY));
+
 		CreateWindowW(L"Static", team, WS_CHILD|WS_VISIBLE|SS_LEFT, 10, 12, 197, 30, hwnd, (HMENU)1, NULL, NULL);
-		hwndEdit=CreateWindowW(L"Edit", NULL, WS_CHILD|WS_VISIBLE|WS_BORDER, 195, 9, 270, 23, hwnd, (HMENU)ID_EDIT, NULL, NULL);
+		hwndTeam=CreateWindowW(L"Edit", NULL, WS_CHILD|WS_VISIBLE|WS_BORDER, 195, 9, 270, 23, hwnd, (HMENU)ID_EDIT, NULL, NULL);
+		hwndButton=CreateWindowW(L"Button", L"", WS_VISIBLE | WS_CHILD | BS_TEXT | BS_BOTTOM, 475, 10, 20, 20, hwnd, (HMENU)ID_BUTTON, hInstance, NULL);
+		
 		CreateWindowW(L"Static", srcdir, WS_CHILD|WS_VISIBLE|SS_LEFT, 10, 40, 197, 30, hwnd, (HMENU)1, NULL, NULL);
-		hwndEdit=CreateWindowW(L"Edit", NULL, WS_CHILD|WS_VISIBLE|WS_BORDER, 195, 37, 270, 23, hwnd, (HMENU)ID_EDIT, NULL, NULL);
+		hwndSrcdir=CreateWindowW(L"Edit", NULL, WS_CHILD|WS_VISIBLE|WS_BORDER, 195, 37, 270, 23, hwnd, (HMENU)ID_EDITB, NULL, NULL);
+		hwndButtonB=CreateWindowW(L"Button", L"", WS_VISIBLE|WS_CHILD, 475, 38, 20, 20, hwnd, (HMENU)ID_BUTTONB, NULL, NULL);
+		
 		CreateWindowW(L"Static", destdir, WS_CHILD|WS_VISIBLE|SS_LEFT, 10, 70, 197, 30, hwnd, (HMENU)1, NULL, NULL);
-		hwndEdit=CreateWindowW(L"Edit", NULL, WS_CHILD|WS_VISIBLE|WS_BORDER, 195, 67, 270, 23, hwnd, (HMENU)ID_EDIT, NULL, NULL);
+		hwndDestdir=CreateWindowW(L"Edit", NULL, WS_CHILD|WS_VISIBLE|WS_BORDER, 195, 67, 270, 23, hwnd, (HMENU)ID_EDITC, NULL, NULL);
+		hwndButtonC=CreateWindowW(L"Button", L"", WS_VISIBLE|WS_CHILD, 475, 68, 20, 20, hwnd, (HMENU)ID_BUTTONC, NULL, NULL);
+		
 		CreateWindowW(L"Static", bufreq, WS_CHILD|WS_VISIBLE|SS_LEFT, 10, 100, 197, 30, hwnd, (HMENU)1, NULL, NULL);
-		hwndEdit=CreateWindowW(L"Edit", NULL, WS_CHILD|WS_VISIBLE|WS_BORDER, 195, 97, 270, 23, hwnd, (HMENU)ID_EDIT, NULL, NULL);
+		hwndBufreq=CreateWindowW(L"Edit", NULL, WS_CHILD|WS_VISIBLE|WS_BORDER, 195, 97, 270, 23, hwnd, (HMENU)ID_EDITD, NULL, NULL);
+		hwndButtonD=CreateWindowW(L"Button", L"", WS_VISIBLE|WS_CHILD, 475, 98, 20, 20, hwnd, (HMENU)ID_BUTTOND, NULL, NULL);
+		
+		//hMyIcon=LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICONY));
+		SendMessage(hwndButton, BM_SETIMAGE, (WPARAM)IMAGE_ICON,(LPARAM)hMyIcon);
+
+		hwndButtonE=CreateWindowW(L"Button", L"Lancer BackUp", WS_VISIBLE|WS_CHILD, 190, 130, 120, 21, hwnd, (HMENU)ID_BUTTONE, NULL, NULL);
 		break;   
 	case WM_COMMAND:
 		if (LOWORD(wParam)==ID_BUTTON) {
@@ -65,7 +87,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 			RedrawWindow(hwndLabel, NULL, NULL, RDW_ERASE);
 		}
 		break;
-	case WM_DESTROY:  
+	case WM_DESTROY: 
+		DestroyIcon((HICON)hMyIcon);
 		PostQuitMessage(0);
 		break;
 	}
---
+++ BackUp_WIn/resource.h
@@ -0,0 +1,13 @@
+#define IDI_MYICON 101
+#define IDI_MYICONY 102
+
+#define ID_BUTTON 1
+#define ID_BUTTONB 2
+#define ID_BUTTONC 3
+#define ID_BUTTOND 4
+#define ID_BUTTONE 5
+
+#define ID_EDIT 20
+#define ID_EDITB 21
+#define ID_EDITC 22
+#define ID_EDITD 23
